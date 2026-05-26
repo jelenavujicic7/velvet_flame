1673447a0c5da1ac3666303bb08b8da12e551822
jelenavujicic7 <jelenavujicic2005@gmail.com>
2026-05-26 11:42:46 +0200

    dodavanje LoginScreen-a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,11 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 import ProductScreen from './screens/ProductScreen';
 import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './styles/bootstrap-custom.css';
 import './styles/App.css';
 
@@ -11,10 +14,12 @@ function App() {
   return (
     <div className="app">
       <Header />
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/product/:id" element={<ProductScreen />} />
         <Route path="/cart" element={<CartScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
       </Routes>
       <Footer />
     </div>
